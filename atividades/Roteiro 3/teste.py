1b6613de8d7146ff6989534aeeb85c7f8bc95cd8
TheGabrielSN <63022715+TheGabrielSN@users.noreply.github.com>
2021-03-18 09:19:40 -0300

    Roteiro 3

--- atividades/Roteiro 3/teste.py
+++ atividades/Roteiro 3/teste.py
@@ -15,16 +15,13 @@ g_p.adicionaAresta('a8', 'M', 'T')
 g_p.adicionaAresta('a9', 'T', 'Z')
 g_p.adicionaAresta('a10', 'Z', 'Z')
 
-#print(list(g_p.bfs("A").A)) -> bibgrafo.grafo_exceptions.VerticeInvalidoException
-
-
-#print("J ->",list(g_p.bfs("J").A))
-#print("C ->",list(g_p.bfs("C").A))
-#print("E ->",list(g_p.bfs("E").A))
-#print("P ->",list(g_p.bfs("P").A))
-#print("M ->",list(g_p.bfs("M").A))
-#print("T ->",list(g_p.bfs("T").A))
-#print("Z ->",list(g_p.bfs("Z").A))
+print("J ->",list(g_p.bfs("J").A))
+print("C ->",list(g_p.bfs("C").A))
+print("E ->",list(g_p.bfs("E").A))
+print("P ->",list(g_p.bfs("P").A))
+print("M ->",list(g_p.bfs("M").A))
+print("T ->",list(g_p.bfs("T").A))
+print("Z ->",list(g_p.bfs("Z").A))
 
 #J -> ['a1', 'a2', 'a4', 'a6', 'a7', 'a9']
 #C -> ['a1', 'a2', 'a4', 'a6', 'a7', 'a9']
@@ -53,17 +50,17 @@ g_nd.adicionaAresta('a15', 'D', 'E')
 g_nd.adicionaAresta('a16', 'B', 'D')
 g_nd.adicionaAresta('a17', 'B', 'E')
 
-#print(list(g_nd.bfs("A").A))
-#print(list(g_nd.bfs("B").A))
-#print(list(g_nd.bfs("C").A))
-#print(list(g_nd.bfs("D").A))
-#print(list(g_nd.bfs("E").A))
-#print(list(g_nd.bfs("F").A))
-#print(list(g_nd.bfs("G").A))
-#print(list(g_nd.bfs("H").A))
-#print(list(g_nd.bfs("I").A))
-#print(list(g_nd.bfs("J").A))
-#print(list(g_nd.bfs("K").A))
+print(list(g_nd.bfs("A").A))
+print(list(g_nd.bfs("B").A))
+print(list(g_nd.bfs("C").A))
+print(list(g_nd.bfs("D").A))
+print(list(g_nd.bfs("E").A))
+print(list(g_nd.bfs("F").A))
+print(list(g_nd.bfs("G").A))
+print(list(g_nd.bfs("H").A))
+print(list(g_nd.bfs("I").A))
+print(list(g_nd.bfs("J").A))
+print(list(g_nd.bfs("K").A))
 #A -> ['a1', 'a2', 'a3', 'a11', 'a13', 'a16', 'a17', 'a10', 'a4', 'a8']
 #B -> ['a1', 'a11', 'a12', 'a13', 'a16', 'a17', 'a3', 'a4', 'a8', 'a9']
 #C -> ['a13', 'a14', 'a1', 'a11', 'a12', 'a17', 'a3', 'a4', 'a8', 'a9']
@@ -75,20 +72,6 @@ g_nd.adicionaAresta('a17', 'B', 'E')
 #I -> ['a7', 'a8', 'a3', 'a5', 'a1', 'a11', 'a13', 'a16', 'a17', 'a10']
 #J -> ['a3', 'a5', 'a6', 'a7', 'a1', 'a11', 'a13', 'a16', 'a17', 'a10']
 #K -> ['a4', 'a5', 'a2', 'a8', 'a9', 'a12', 'a11', 'a13', 'a16', 'a17']
-
-#['a1', 'a2', 'a3', 'a11', 'a13', 'a16', 'a17', 'a10', 'a4', 'a8']
-#['a1', 'a11', 'a12', 'a13', 'a16', 'a17', 'a3', 'a4', 'a8', 'a9']
-#['a13', 'a14', 'a1', 'a11', 'a12', 'a17', 'a3', 'a4', 'a8', 'a9']
-#['a14', 'a15', 'a16', 'a1', 'a11', 'a12', 'a3', 'a4', 'a8', 'a9']
-#['a15', 'a17', 'a14', 'a1', 'a11', 'a12', 'a3', 'a4', 'a8', 'a9']
-#['a10', 'a11', 'a9', 'a2', 'a4', 'a6', 'a8', 'a13', 'a16', 'a17']
-#['a2', 'a4', 'a6', 'a8', 'a9', 'a12', 'a11', 'a13', 'a16', 'a17']
-#['a9', 'a10', 'a2', 'a4', 'a6', 'a8', 'a12', 'a13', 'a16', 'a17']
-#['a7', 'a8', 'a3', 'a5', 'a1', 'a11', 'a13', 'a16', 'a17', 'a10']
-#['a3', 'a5', 'a6', 'a7', 'a1', 'a11', 'a13', 'a16', 'a17', 'a10']
-#['a4', 'a5', 'a2', 'a8', 'a9', 'a12', 'a11', 'a13', 'a16', 'a17']
-
-
 
 g_l1 = MeuGrafo(['A', 'B', 'C', 'D'])
 g_l1.adicionaAresta('a1', 'A', 'A')
